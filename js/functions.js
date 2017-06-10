c3f2ed3011245669fa5ee48dc1a8c796b75085f9
Michał Kozłowski <kozlowskimichal88@gmail.com>
2017-06-10 14:43:39 +0200

    pierwsza funkcja

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+function multiply(parametr1, parametr2, parametr3){
+    ver result;
+    
+    result = parametr1 + parametr2 + parametr3;
+    
+    retrun result;
+}
+
+var wyniki = multiply(3, 9, 10);
+
+console.log("Wynik: " + wynik );
